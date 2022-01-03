934946ad2ed3fb4293c2104a5e0faceb573adfce
fan <yficjg@gmail.com>
2022-01-03 18:32:51 +0100

    stream data, but with error

--- app/src/main/java/com/example/demoqplus/QplusActivity.kt
+++ app/src/main/java/com/example/demoqplus/QplusActivity.kt
@@ -3,25 +3,40 @@ package com.example.demoqplus
 import android.Manifest
 import android.bluetooth.BluetoothAdapter
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.appcompat.widget.SwitchCompat
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.example.demoqplus.databinding.ActivityQplusBinding
+import com.github.mikephil.charting.charts.LineChart
 import config.ConnectionConfig
+import config.RecordConfig
+import config.StreamConfig
+import core.bluetooth.StreamState
+import core.device.event.DCOffsetEvent
+import core.device.event.SaturationEvent
+import core.device.event.indus5.RecordingSavedListener
 import core.device.model.MbtDevice
+import core.eeg.storage.MbtEEGPacket
 import engine.MbtClient
-import engine.clientevents.BaseError
-import engine.clientevents.ConnectionStateListener
-import engine.clientevents.DeviceBatteryListener
+import engine.clientevents.*
 import features.MbtDeviceType
 import timber.log.Timber
+import utils.MatrixUtils
+import java.util.*
+import kotlin.collections.ArrayList
 
 
-class QplusActivity : AppCompatActivity(), ConnectionStateListener<BaseError> {
+class QplusActivity : AppCompatActivity(), ConnectionStateListener<BaseError>,
+    EegListener<BaseError>, DeviceStatusListener<BaseError> {
 
     // Declare bluetooth permissions
     var PERMISSIONS = arrayOf(
@@ -35,20 +50,77 @@ class QplusActivity : AppCompatActivity(), ConnectionStateListener<BaseError> {
     // bluetooth
     val REQUEST_ENABLE_BT: Int = 1
     lateinit var mbtClient: MbtClient
+
+    // quality check
+    private lateinit var binding: ActivityQplusBinding
+    var lastQualities = ArrayList<LinkedList<Float>>()
+    var qualityWindowLength = 6
+    var channelNb = 4
+    var qualityButtons = ArrayList<Button>()
+    var channelFlags = ArrayList<Boolean>().apply {
+        for (i in 1..channelNb) {
+            this.add(false)
+        }
+    }
+    var recordingSavedListener: RecordingSavedListener = object : RecordingSavedListener {
+        override fun onRecordingSaved(recordConfig: RecordConfig) {
+            Timber.d("onRecordingSaved")
+        }
+    }
+
     // test case only
     var isClientExisted: Boolean = false
+    var IS_STREAMING: Boolean = false
+    var IS_DEVICE_CONNECTED:Boolean = false
     val KEY_DEVICE_TYPE = MbtDeviceType.MELOMIND_Q_PLUS
+    val KEY_P300: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qplus)
+
+        binding = ActivityQplusBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //var mLineChart = findViewById<LineChart>(R.id.chart)
+        // add buttons to list
+        qualityButtons.add(binding.P3)
+        qualityButtons.add(binding.P4)
+        qualityButtons.add(binding.AF3)
+        qualityButtons.add(binding.AF4)
+        for (i in 1..channelNb) {
+            lastQualities.add(LinkedList())
+        }
+
+        var streamStateChange = findViewById<Button>(R.id.button_start_stop_stream)
+        streamStateChange.setOnClickListener{
+            if (!IS_STREAMING){
+                // start stream if it isn't streamin
+                if (IS_DEVICE_CONNECTED) {
+                    streamStateChange.setBackgroundColor(getResources().getColor(R.color.red))
+                    streamStateChange.setText("Stop Stream")
+                    onStartStreamButtonClicked()
+                    IS_STREAMING = true
+                } else {
+                    Toast.makeText(this, "device not connected", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                // stop
+                mbtClient.stopStream()
+                streamStateChange.setBackgroundColor(getResources().getColor(R.color.white))
+                streamStateChange.setText("Start Stream")
+                IS_STREAMING = false
+            }
+
+        }
 
 
         checkPermission()
 
         val btnFinish = findViewById<Button>(R.id.button_finish)
         btnFinish.setOnClickListener{
-            if (isClientExisted) {
+            if (isClientExisted && IS_DEVICE_CONNECTED) {
                 mbtClient.disconnectBluetooth()
             }
             finish()
@@ -59,13 +131,17 @@ class QplusActivity : AppCompatActivity(), ConnectionStateListener<BaseError> {
             if (isChecked) {
                 if (PERMISSION_GRANTED) {
                     connectMBTdevice()
+                    streamStateChange.isEnabled = true
                 } else {
                     switchDevice.isChecked = false
                     checkPermission()
                     Toast.makeText(this, "Permissions are denied", Toast.LENGTH_SHORT).show()
                 }
             } else {
-                mbtClient.disconnectBluetooth()
+                if (IS_DEVICE_CONNECTED){
+                    mbtClient.disconnectBluetooth()
+                    streamStateChange.setText("Start Stream")
+                }
             }
         }
     }
@@ -157,12 +233,55 @@ class QplusActivity : AppCompatActivity(), ConnectionStateListener<BaseError> {
     override fun onDeviceConnected(p0: MbtDevice?) {
         // device is connected
         Timber.d("Device is connected")
+        IS_DEVICE_CONNECTED = true
         readBattery()
     }
 
     override fun onDeviceDisconnected(p0: MbtDevice?) {
         //device is disconnected
         Timber.d("Device is disconnected")
+        mbtClient.stopStream()
+        IS_STREAMING = false
+        IS_DEVICE_CONNECTED = false
+
+    }
+
+
+    private fun onStartStreamButtonClicked(){
+        Timber.d("onStartStreamButtonClicked")
+        startStreamWithTrigger(KEY_P300, false)
+
+    }
+
+    private fun startStreamWithTrigger(isTriggerEnabled: Boolean, ppgEnabled: Boolean) {
+
+        val streamConfigBuilder = StreamConfig.Builder(this@QplusActivity)
+            .useQualities()
+            .setDeviceStatusListener(this@QplusActivity)
+        val streamConfig = streamConfigBuilder.createForDevice(MbtDeviceType.MELOMIND_Q_PLUS)
+        streamConfig.recordingSavedListener = recordingSavedListener
+        streamConfig.isImsEnabled = false
+        Timber.d("ppgEnabled = $ppgEnabled")
+        streamConfig.isPpgEnabled = ppgEnabled
+        streamConfig.isTriggerEnabled = isTriggerEnabled
+        mbtClient.startStream(streamConfig)
+        Timber.d("startStreamWithTrigger")
+    }
+
+
+
+    override fun onNewPackets(mbteegPackets: MbtEEGPacket) {
+        Timber.d("onNewPackets")
+    }
+
+    override fun onNewStreamState(streamState: StreamState) {}
+
+    override fun onSaturationStateChanged(p0: SaturationEvent?) {
+        //TODO("Not yet implemented")
+    }
+
+    override fun onNewDCOffsetMeasured(p0: DCOffsetEvent?) {
+        //
     }
 
 }
