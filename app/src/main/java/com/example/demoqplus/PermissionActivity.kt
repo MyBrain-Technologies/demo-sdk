dca01b3a26363b558b5eb3ba63fc9f26755b9101
mbt-AnhTuan-M <anhtuan.mai@mybraintech.com>
2022-09-19 23:54:57 +0200

    add permission for Android 12

--- app/src/main/java/com/example/demoqplus/PermissionActivity.kt
+++ app/src/main/java/com/example/demoqplus/PermissionActivity.kt
@@ -111,6 +111,7 @@ class PermissionActivity : AppCompatActivity() {
                 arrayOf(
                     Manifest.permission.WRITE_EXTERNAL_STORAGE,
                     Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.BLUETOOTH_CONNECT,
                     Manifest.permission.BLUETOOTH_SCAN
                 )
